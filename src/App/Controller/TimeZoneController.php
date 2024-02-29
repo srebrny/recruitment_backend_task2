bb26496c331a553a5282ca4f56feabc163818f5a
Paweł Srebniak <info@pycode.biz>
2024-02-29 12:32:41 +0100

    [*] TimeZone: Update templates, forms and tests.

--- src/App/Controller/TimeZoneController.php
+++ src/App/Controller/TimeZoneController.php
@@ -2,19 +2,63 @@
 
 namespace App\Controller;
 
+use App\Form\TimeZoneType;
+use DateTime;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\b;
 
 class TimeZoneController extends AbstractController
 {
     /**
      * @Route("/time/zone", name="app_time_zone")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(TimeZoneType::class, [], ['attr' => ['id' => 'timezone_form']]);
+        $form->handleRequest($request);
+        $inputTimeZone = "";
+        $timeZoneOffset = "";
+        $numberOfDaysInFebruary = "";
+        $fullCurrentMonthName = "";
+        $numDaysOfCurrentMonth = "";
+        if ($form->isSubmitted()) {
+            $timeZoneForm = $form->getData();
+            $inputTimeZone = $timeZoneForm["timezone"];
+
+            if (!in_array($timeZoneForm["timezone"], DateTimeZone::listIdentifiers(), true)) {
+                $form
+                    ->get('timezone')
+                    ->addError(new FormError('Wrong timezone!'));
+            }
+            $dtTimeZone = new DateTimeZone($inputTimeZone);
+            $dt = DateTime::createFromFormat("Y-m-d 23:59:59", $timeZoneForm["date"]."23:59:59", $dtTimeZone);
+            if (!$dt || array_sum($dt::getLastErrors())) {
+                $form
+                    ->get('date')
+                    ->addError(new FormError('Wrong date format use Y-m-d'));
+            }
+            $timeZoneOffset = $dt->getOffset() / 60;
+            $fullCurrentMonthName = $dt->format('F');
+            $dt->modify("last day of ". $fullCurrentMonthName);
+            $numDaysOfCurrentMonth = $dt->format("d");
+
+            $dt->modify("last day of February");
+            $numberOfDaysInFebruary = $dt->format("d");
+        }
+
         return $this->render('time_zone/index.html.twig', [
             'controller_name' => 'TimeZoneController',
+            'form' => $form->createView(),
+            "input_timezone" => $inputTimeZone,
+            "timeZoneOffset" => $timeZoneOffset,
+            "numberOfDaysInFebruary" => $numberOfDaysInFebruary,
+            "fullCurrentMonthName" => $fullCurrentMonthName,
+            "numDaysOfCurrentMonth" => $numDaysOfCurrentMonth,
         ]);
     }
 }
--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -1,6 +1,10 @@
 <?php
+
 namespace App\Tests;
-use App\Tests\AcceptanceTester;
+
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
+
 class FirstCest
 {
     public function _before(AcceptanceTester $I)
@@ -8,9 +12,62 @@ class FirstCest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function checkHeaderTest(AcceptanceTester $I)
     {
         $I->amOnPage('/time/zone');
         $I->see('Hello TimeZoneController');
+        $I->cantSee('The time zone [input-timezone] has [A] minutes offset to UTC');
+        $I->cantSee('February in this year is [B] days long.');
+        $I->cantSee('The specified month [C] is [D] days long.');
+    }
+
+    protected function pageProvider(): array
+    {
+        return [
+            [
+                'input_date' => "2019-07-10",
+                "input_timezone" => "Europe/London",
+                "timeZoneOffset" => "+60",
+                "numberOfDaysInFebruary" => "28",
+                "fullCurrentMonthName" => "July",
+                "numDaysOfCurrentMonth" => "31",
+            ],
+            [
+                'input_date' => "2016-11-28",
+                "input_timezone" => "Asia/Tokyo",
+                "timeZoneOffset" => "+540",
+                "numberOfDaysInFebruary" => "29",
+                "fullCurrentMonthName" => "November",
+                "numDaysOfCurrentMonth" => "30",
+            ],
+            [
+                'input_date' => "1853-01-30",
+                "input_timezone" => "America/Lower_Princes",
+                "timeZoneOffset" => "-240",
+                "numberOfDaysInFebruary" => "28",
+                "fullCurrentMonthName" => "January",
+                "numDaysOfCurrentMonth" => "31",
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider pageProvider
+     */
+    public function checkTimeZoneTextTest(AcceptanceTester $I, Example $example)
+    {
+        $I->amOnPage('/time/zone');
+        $I->submitForm('#timezone_form', array(
+                'form' => array(
+                    'date' => $example['input_date'],
+                    'timezone' => $example['input_timezone'],
+                )
+            )
+        );
+
+        $I->see('The time zone '.$example['input_timezone']);
+        $I->see('has '.$example['timeZoneOffset'].' minutes offset to UTC');
+        $I->see('February in this year is '.$example['numberOfDaysInFebruary'].' days long.');
+        $I->see('The specified month '.$example['fullCurrentMonthName'].' is '.$example['numDaysOfCurrentMonth'].' days long.');
     }
 }
